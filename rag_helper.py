aeda384740e2187daeb4ec3889faab701536b0e8
aksingh-08 <ashishk.singh1008@gmail.com>
2026-06-21 22:05:37 +0530

    modified

--- rag_helper.py
+++ rag_helper.py
@@ -1,55 +1,62 @@
 INSTRUCTIONS = '''
 Your task is to answer questions from the course participants
-based on the provided content.
+based on the provided context.
 
 Use the context to find relevant information and provide accurate
 answers. If the answer is not found in the context,
-respond with "I don't know.
+respond with "I don't know."
 '''
 
 PROMPT_TEMPLATE = '''
 QUESTION: {question}
 
 CONTEXT:
-    {context}
-    '''.strip()
+{context}
+'''.strip()
+
 
 class RAGBase:
+
     def __init__(
         self,
         index,
         llm_client,
         instructions=INSTRUCTIONS,
         prompt_template=PROMPT_TEMPLATE,
-        course='llm-zoomcamp',
+        # course='llm-zoomcamp',
         model='llama-3.3-70b-versatile'
     ):
         self.index = index
         self.llm_client = llm_client
         self.instructions = instructions
-        self.course = course
+        # self.course = course
         self.prompt_template = prompt_template
         self.model = model
 
     def search(self, query, num_results=5):
-        boost_dict = {'question': 3.0, 'section': 0.5}
-        filter_dict = {'course': self.course}
+        # boost_dict = {'question': 3.0, 'section': 0.5}
+        # filter_dict = {'course': self.course}
 
         return self.index.search(
             query,
             num_results=num_results,
-            boost_dict=boost_dict,
-            filter_dict=filter_dict
+            # boost_dict=boost_dict,
+            # filter_dict=filter_dict
         )
 
     def build_context(self, search_results):
         lines = []
 
         for doc in search_results:
-            lines.append(doc['section'])
-            lines.append('Q: ' + doc['question'])
-            lines.append('A: ' + doc['answer'])
-            lines.append('---')
+            # lines.append(doc['section'])
+            # lines.append('Q: ' + doc['question'])
+            # lines.append('A: ' + doc['answer'])
+            # lines.append('')
+
+            lines.append(doc['filename'])
+            lines.append(doc['content'])
+            lines.append('')
+
         return '\n'.join(lines).strip()
 
     def build_prompt(self, query, search_results):
@@ -68,10 +75,17 @@ class RAGBase:
             model=self.model,
             input=input_messages
         )
-        return response.output_text
+
+        # return response.output_text
+        return response
 
     def rag(self, query):
         search_results = self.search(query)
         prompt = self.build_prompt(query, search_results)
-        answer = self.llm(prompt)
-        return answer
+        # answer = self.llm(prompt)
+        response = self.llm(prompt)
+
+        answer = response.output_text
+        usage = response.usage
+        
+        return answer, usage
