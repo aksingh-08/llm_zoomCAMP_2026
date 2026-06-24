a82e2c874421dd53cf247f45f3075dc2577d45fe
aksingh-08 <ashishk.singh1008@gmail.com>
2026-06-24 22:18:08 +0530

    module 1 hw done, file changed back for further module learning

--- rag_helper.py
+++ rag_helper.py
@@ -23,40 +23,35 @@ class RAGBase:
         llm_client,
         instructions=INSTRUCTIONS,
         prompt_template=PROMPT_TEMPLATE,
-        # course='llm-zoomcamp',
+        course='llm-zoomcamp',
         model='llama-3.3-70b-versatile'
     ):
         self.index = index
         self.llm_client = llm_client
         self.instructions = instructions
-        # self.course = course
+        self.course = course
         self.prompt_template = prompt_template
         self.model = model
 
     def search(self, query, num_results=5):
-        # boost_dict = {'question': 3.0, 'section': 0.5}
-        # filter_dict = {'course': self.course}
+        boost_dict = {'question': 3.0, 'section': 0.5}
+        filter_dict = {'course': self.course}
 
         return self.index.search(
             query,
             num_results=num_results,
-            # boost_dict=boost_dict,
-            # filter_dict=filter_dict
+            boost_dict=boost_dict,
+            filter_dict=filter_dict
         )
 
     def build_context(self, search_results):
         lines = []
 
         for doc in search_results:
-            # lines.append(doc['section'])
-            # lines.append('Q: ' + doc['question'])
-            # lines.append('A: ' + doc['answer'])
-            # lines.append('')
-
-            lines.append(doc['filename'])
-            lines.append(doc['content'])
+            lines.append(doc['section'])
+            lines.append('Q: ' + doc['question'])
+            lines.append('A: ' + doc['answer'])
             lines.append('')
-
         return '\n'.join(lines).strip()
 
     def build_prompt(self, query, search_results):
@@ -70,22 +65,14 @@ class RAGBase:
             {'role': 'developer', 'content': self.instructions},
             {'role': 'user', 'content': prompt}
         ]
-
         response = self.llm_client.responses.create(
             model=self.model,
             input=input_messages
         )
-
-        # return response.output_text
-        return response
+        return response.output_text
 
     def rag(self, query):
         search_results = self.search(query)
         prompt = self.build_prompt(query, search_results)
-        # answer = self.llm(prompt)
-        response = self.llm(prompt)
-
-        answer = response.output_text
-        usage = response.usage
-        
-        return answer, usage
+        answer = self.llm(prompt)
+        return answer
